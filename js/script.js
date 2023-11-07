1db40fac4ab25366fb3323d2995c15ffb8f79b17
kuliznikovjeka <99824934+kuliznikovjeka@users.noreply.github.com>
2023-11-07 10:30:55 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('btn');
+const result = document.getElementById('result');
+
+btn.addEventListener('click', () => {
+	result.textContent = 'Cобытие клика навешено!'
+})
