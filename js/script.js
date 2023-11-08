5351f73061429d065a6f7f7561164326da1b6516
kuliznikovjeka <99824934+kuliznikovjeka@users.noreply.github.com>
2023-11-08 10:34:24 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,63 @@
+const firstInput = document.getElementById('first-input-value');
+const secondInput = document.getElementById('second-input-value');
+const selectedAction = document.getElementById('selected-action');
+const addition = document.getElementById('addition');
+const subtraction = document.getElementById('subtraction');
+const multiplication = document.getElementById('multiplication');
+const division = document.getElementById('division');
 const btn = document.getElementById('btn');
-const result = document.getElementById('result');
+let result = document.getElementById('result');
+
+const additionNumbers = () => {
+	return result.textContent = `Итого: ${+firstInput.value + +secondInput.value}`;
+}
+
+const substractNumbers = () => {
+	return result.textContent = `Итого: ${firstInput.value - +secondInput.value}`;
+}
+
+const multiplyNumbers = () => {
+	return result.textContent = `Итого: ${+firstInput.value * +secondInput.value}`;
+}
+
+const divideNumbers = () => {
+	if (+secondInput.value === 0) {
+		return result.textContent = 'ОШИБКА! На ноль делить нельзя'
+	} else {
+		return result.textContent = `Итого: ${+firstInput.value / +secondInput.value}`;
+	}
+}
+
+const renderValues = () => {
+	firstInput.value = '';
+	secondInput.value = '';
+}
 
 btn.addEventListener('click', () => {
-	result.textContent = 'Cобытие клика навешено!'
-})
+	if (firstInput.value.length === 0 || secondInput.value.length === 0) return;
+
+	switch (selectedAction.textContent) {
+
+		case 'сложить +':
+			additionNumbers();
+			break;
+
+		case 'вычесть -':
+			substractNumbers();
+			break;
+
+		case 'умножить *':
+			multiplyNumbers();
+			break;
+
+		case 'разделить /':
+			divideNumbers();
+			break;
+
+		default:
+			additionNumbers();
+
+	}
+
+	renderValues();
+});
